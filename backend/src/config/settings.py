bd5ff1072b0436545eeab8c917a980b40fb55fe1
maksimzhohov <zhohovmaksim@gmail.com>
2024-08-12 00:00:10 +0300

    feat: add database settings

---
+++ backend/src/config/settings.py
@@ -0,0 +1,24 @@
+import os
+from dataclasses import dataclass, field
+
+
+@dataclass
+class DatabaseSettings:
+    db_name: str = field(default=os.getenv("DB_NAME", "example"))
+    db_user: str = field(default=os.environ.get("DB_USER", "postgres"))
+    db_password: str = field(default=os.getenv("DB_PASSWORD", "postgres"))
+    db_host: str = field(default=os.getenv("DB_HOST", "db"))
+    db_port: str = field(default=os.getenv("DB_PORT", "5432"))
+
+    @property
+    def url(self) -> str:
+        return f"postgresql+asyncpg://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"  # noqa: E501
+
+
+@dataclass
+class Settings:
+    db: DatabaseSettings = field(default_factory=DatabaseSettings)
+
+
+settings = Settings()
+
