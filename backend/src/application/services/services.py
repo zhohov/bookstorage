5bcfe961aca3f1d35db9b6a4dac2e7f2df9cafc5
maksimzhohov <zhohovmaksim@gmail.com>
2024-08-29 23:53:12 +0300

    feat: add AbstractService

--- backend/src/application/services/services.py
+++ backend/src/application/services/services.py
@@ -1,12 +1,30 @@
-from typing import Any, List, Optional, Dict
+from abc import ABC, abstractmethod
+from typing import Any, Generic, List, Optional, TypeVar
 from sqlalchemy.orm import Session
 from src.domain.entities import Author # pyright: ignore
 from src.domain.repository import AbstractRepository # pyright: ignore
 
-from src.application.dto.dto import AuthorInput # pyright: ignore
+from src.application.dto.dto import BaseDTO, AuthorInput # pyright: ignore
 
 
-class AuthorService:
+T = TypeVar("T", bound=BaseDTO)
+
+
+class AbstractService(ABC, Generic[T]):
+    @abstractmethod
+    def create(self, payload: T) -> Optional[T]:
+        ...
+
+    @abstractmethod
+    def get(self, key: str, value: Any) -> Optional[T]:
+        ...
+
+    @abstractmethod
+    def all(self) -> Optional[List[T]]:
+        ...
+
+
+class AuthorService(AbstractService[T]):
     def __init__(
         self, session: Session, repository: AbstractRepository
     ) -> None:
