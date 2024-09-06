00eed4d4ab459fcc3749bdc78471d0ee09ea2582
maksimzhohov <zhohovmaksim@gmail.com>
2024-09-06 23:28:43 +0300

    feat(domain): add validation for value objects

--- backend/src/domain/value_objects.py
+++ backend/src/domain/value_objects.py
@@ -11,18 +11,46 @@ class FullName(BaseValueObject):
     first_name: str
     last_name: str
 
+    def validate(self) -> None:
+        if len(self.first_name) > 64 or len(self.last_name) > 64:
+            raise ValueError()
+
+    def __post_init__(self) -> None:
+        self.validate()
+
 
 @dataclass(frozen=True)
 class Title(BaseValueObject):
     value: str
+
+    def validate(self) -> None:
+        if len(self.value) > 128:
+            raise ValueError()
+
+    def __post_init__(self) -> None:
+        self.validate()
 
 
 @dataclass(frozen=True)
 class Description(BaseValueObject):
     value: str
 
+    def validate(self) -> None:
+        if len(self.value) > 512:
+            raise ValueError()
+
+    def __post_init__(self) -> None:
+        self.validate()
+
 
 @dataclass(frozen=True)
 class ISBN(BaseValueObject):
     value: str
+
+    def validate(self) -> None:
+        if len(self.value) > 13 or not self.value.isdigit:
+            raise ValueError()
+
+    def __post_init__(self) -> None:
+        self.validate()
 
