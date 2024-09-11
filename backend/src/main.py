bdbec68bd8ec27fc357f6c6158aebcbd9b6f2e21
maksimzhohov <zhohovmaksim@gmail.com>
2024-09-11 17:48:46 +0300

    feat: add fastapi startup method

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,12 +2,20 @@ import uvicorn
 from fastapi import FastAPI
 
 from infrastructure.application.factory import create_app
+from infrastructure.persistence.tables import start_mappers, metadata
+from infrastructure.persistence.session import get_engine
 
 
 app: FastAPI = create_app(
     title="bookstorage",
     routers=[],
 )
+
+
+@app.on_event("startup")
+def startup_event() -> None:
+    metadata.create_all(bind=get_engine())
+    start_mappers()
 
 
 if __name__ == "__main__":
